25828fe6a6db33202058afb66d1e381306f9e0bb
EsmeraldaMTZV <23308060610235@cetis61.edu.mx>
2025-11-12 10:00:24 -0600

    actualizar

--- app.py
+++ app.py
@@ -14,12 +14,34 @@ def search_pokemon():
     pokemon_name = request.form.get ('pokemon_name', '').strip().lower()
     
     if not pokemon_name:
-        flash('Ingresa un nombre correcto de Pokemon')
+        flash('Ingresa un nombre correcto de Pokemon', 'error')
         return redirect(url_for('index'))
     
-        resp= request.get(f"{API}{pokemon_name}")
+    try:
+        resp = requests.get(f"{API}{pokemon_name}")
         if resp.status_code == 200:
-            pokemon_data= resp.json()
+            pokemon_data = resp.json()
+            
+            pokemon_info = {
+                'name' : pokemon_data['name'].title()
+                'id': pokemon_data['id']
+                'height' : pokemon_data['height']/10
+                'weight' : pokemon_data['weight']/10
+                'image' : pokemon_data['sprites']['']
+                'types' : [t['type']['name'].title() for t in pokemon_data['type']]
+                'abilities' : [a['abilitiy']['name'].title() for t in pokemon_data['abilities']]
+                
+            }
+        
+        return render_template('pokemon_html',pokemon = pokemon_data)
+    
+    else:
+    flash(f'Pokémon"{pokemon_name}"no encontrado','error')
+    return redirect(url_for('index'))
+    
+    except requests.exceptions.RequestException as e:
+    flash('Error al buscar el Pokemon','error')
+    return redirect(url_for('index'))
     
     if __name__ == "__main__":
         app.run(debug=True)
